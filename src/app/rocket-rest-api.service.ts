1a21d0c1442344cfd1b810fd7eb581124190776d
Aarón <aaronarrieropelaez99@gmail.com>
2019-05-10 18:23:01 +0200

    Moved backendUrl to a configuration file and fixed wrong displaying for network errors.

--- src/app/rocket-rest-api.service.ts
+++ src/app/rocket-rest-api.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Response } from './response';
+import { backendUrl } from '../config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RocketRestApiService {
-  baseApiUrl: string = 'http://localhost:8080';
+  baseApiUrl: string = backendUrl;
   errorMsg: string = 'Network Error. Make sure that you have the rest api server active and that you have configured correctly the configuration file with its url. If it\'s everything configured correctly, please, try again.';
 
   getStatus(): Observable<Response> {
@@ -30,7 +31,7 @@ export class RocketRestApiService {
 
   private handleError(operation: string) {
     return (error: any): Observable<null> => {
-      if(error.error instanceof ErrorEvent) {
+      if(error.status !== 409) {
         console.error(`Error trying to connect with the rest api. Operation failed: ${operation}`);
         return of(null);
       } else {
