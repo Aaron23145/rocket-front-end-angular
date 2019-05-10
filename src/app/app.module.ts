5667b1de8aec03c2d77bef7d825c445357f77427
Aarón <aaronarrieropelaez99@gmail.com>
2019-05-10 02:10:25 +0200

    Created navbar and navbar-item components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarItemComponent } from './navbar-item/navbar-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarItemComponent
   ],
   imports: [
     BrowserModule
